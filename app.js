e13eaf8381f2f6c7d45cd702bb97e94f68ecd243
sf-kiwi-cpat <61746287+sf-kiwi-cpat@users.noreply.github.com>
2021-04-24 11:53:07 -0400

    Fixed bug with client v say
    Variable is named 'say' in this, copy and paste error from example code using client

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ app.message('hello', async ({ message, say }) => {
   
   try {
     // Call reactions.add with the built-in client
-    const result = await client.reactions.add({
+    const result = await say.reactions.add({
       token: process.env.BOT_TOKEN,
       channel: message.channel,
       name: 'thumbsup',
