7603860a9ba066d9bbf3a65c560ca2ba7e20ef2b
sf-kiwi-cpat <61746287+sf-kiwi-cpat@users.noreply.github.com>
2021-12-01 11:32:18 -0500

    Removing socket info

--- app.js
+++ app.js
@@ -22,8 +22,6 @@ const web = new WebClient(process.env.SLACK_BOT_TOKEN);
 const app = new App({
 	token: process.env.SLACK_BOT_TOKEN,
 	signingSecret: process.env.SLACK_SIGNING_SECRET,
-	socketMode: true,
-	appToken: process.env.SLACK_APP_TOKEN,
 	// Socket Mode doesn't listen on a port, but in case you want your app to respond to OAuth,
 	// you still need to listen on some port!
 	port: process.env.PORT || 3000
