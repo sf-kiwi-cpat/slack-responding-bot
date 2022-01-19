7761e83904b6b4460f2a711fe0cd95807bd880bb
sf-kiwi-cpat <61746287+sf-kiwi-cpat@users.noreply.github.com>
2022-01-19 09:24:05 -0500

    Adding handling of AppHome Opened

--- app.js
+++ app.js
@@ -27,6 +27,60 @@ const app = new App({
 	port: process.env.PORT || 3000
 });
 
+// App Home opened - response
+app.event('app_home_opened', async ({ event, client, context }) => {
+  try {
+    /* view.publish is the method that your app uses to push a view to the Home tab */
+    const result = await client.views.publish({
+
+      /* the user that opened your app's app home */
+      user_id: event.user,
+
+      /* the view object that appears in the app home*/
+      view: {
+        type: 'home',
+        callback_id: 'home_view',
+
+        /* body of the view */
+        blocks: [
+          {
+            "type": "section",
+            "text": {
+              "type": "mrkdwn",
+              "text": "*Welcome to your _App's Home_* :tada:"
+            }
+          },
+          {
+            "type": "divider"
+          },
+          {
+            "type": "section",
+            "text": {
+              "type": "mrkdwn",
+              "text": "This button won't do much for now but you can set up a listener for it using the `actions()` method and passing its unique `action_id`. See an example in the `examples` folder within your Bolt app."
+            }
+          },
+          {
+            "type": "actions",
+            "elements": [
+              {
+                "type": "button",
+                "text": {
+                  "type": "plain_text",
+                  "text": "Click me!"
+                }
+              }
+            ]
+          }
+        ]
+      }
+    });
+  }
+  catch (error) {
+    console.error(error);
+  }
+});
+
 // Constants for the responses when buttons are clicked.
 const BOT_RESPONSE_HELPED = "Glad I could help, happy selling!";
 const BOT_RESPONSE_HELPED_EMOTICON = "white_check_mark";
